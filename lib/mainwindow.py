6fd0a9ad41357e9741f6f2acbfda1d16560ada71
elushka <elushka@bu.edu>
2017-12-13 04:12:44 -0500

    H

--- lib/mainwindow.py
+++ lib/mainwindow.py
@@ -1,17 +1,13 @@
 import simplejson, socket, sys, pyglet
 import settings
 import game
-from pyglet.gl import *
 
 class MainWindow(pyglet.window.Window):
     keys = None
     game = None
 
-    img1 = pyglet.image.load(settings.FIELD_IMG)
-
     def __init__(self, *args, **kwargs):
         pyglet.window.Window.__init__(self, *args, **kwargs)
-
         self.game = game.Game(multiplayer_mode=True)
         self.keys = pyglet.window.key.KeyStateHandler()
         self.push_handlers(self.keys)
@@ -19,7 +15,7 @@ class MainWindow(pyglet.window.Window):
     def parse_keys(self):
         if self.keys[pyglet.window.key.UP] and self.game.racket_me.y < settings.WINDOW_HEIGHT:
             self.game.racket_me.move(0, settings.MOVE_SPEED)
-
+            
         if self.keys[pyglet.window.key.DOWN] and self.game.racket_me.y > 0:
             self.game.racket_me.move(0, -settings.MOVE_SPEED)
 
@@ -27,7 +23,6 @@ class MainWindow(pyglet.window.Window):
         self.clear()
         self.parse_keys()
 
-        self.img1.blit(0, 0)
         self.game.draw()
 
         self.game.ball.draw()
